6f220299e596da6f2009c0cf2f3332bcd7878aa9
ledtorch <jerry@morispace.com>
2023-08-19 03:24:43 +0800

    BizHr Update

--- app/src/components/Sheet/Businesshour.vue
+++ app/src/components/Sheet/Businesshour.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <p :class="[businessHoursClass, 'subhead']">{{ businessHours }}</p>
-    <!-- <p>{{ currentMonthandDay }}</p> -->
+    <p :class="['subhead', bizHr.stateClass]">{{ bizHr.state }}</p>
+    <p :class="['subhead', bizHr.timeClass]" v-if="bizHr.isTime">
+      &nbsp; · &nbsp;{{ bizHr.time }}
+    </p>
+    <p :class="['subhead', bizHr.nextTimeClass]" v-if="bizHr.isNextTime">
+      &nbsp; · &nbsp;{{ bizHr.nextTime }}
+    </p>
   </div>
 </template>
   
@@ -11,8 +16,7 @@ export default {
     store: Object,
   },
   computed: {
-    businessHours() {
-      // ↓ Define time
+    bizHr() {
       const currentTime = new Date();
       let currentDay = currentTime.getDay(); // Sunday = 0
       if (currentDay === 0) currentDay = 7; // If Sunday, make it 7
@@ -20,6 +24,17 @@ export default {
       const currentDate = String(currentTime.getDate()).padStart(2, "0");
       const currentHour = String(currentTime.getHours()).padStart(2, "0");
       const currentMinute = String(currentTime.getMinutes()).padStart(2, "0");
+      // Define time
+
+      let state = "";
+      let time = "";
+      let isTime = false;
+      let nextTime = "";
+      let isNextTime = false;
+      let stateClass = "";
+      let timeClass = "";
+      let nextTimeClass = "";
+      // Initialization
 
       // // ↓ 🐞 Debug console
       // console.log("Day: " + currentDay)
@@ -29,67 +44,77 @@ export default {
       // console.log("Minute: " + currentMinute)
 
       if (this.store.businesshour === null) {
-        return "Open 24H";
+        state = "Open 24H";
+        stateClass = "isOpen";
+      } else if (
+        this.store.businesshour[0] &&
+        this.store.businesshour[0].Holiday === null
+      ) {
+        state = "Open";
+        stateClass = "isOpen";
+      } else if (
+        this.store.businesshour[0] &&
+        this.store.businesshour[0].Holiday !== null
+      ) {
+        const holidays = this.store.businesshour[0].Holiday;
+        // console.log("Holidays: " + holidays); // ← 🐞 Debug console
+        // Extract holiday
+
+        // ↓ Check if the business is closed for the holiday
+        const currentMonthandDay = currentMonth + currentDate;
+        const bizDay = this.store.businesshour[currentDay];
+        if (holidays.includes(currentMonthandDay)) {
+          // Check for next open day after holiday
+          let nextOpenDate = new Date(currentTime);
+          let nextMonthAndDate;
+          do {
+            nextOpenDate.setDate(nextOpenDate.getDate() + 1); // Increment date by 1
+            nextMonthAndDate =
+              String(nextOpenDate.getMonth() + 1).padStart(2, "0") +
+              String(nextOpenDate.getDate()).padStart(2, "0");
+          } while (holidays.includes(nextMonthAndDate));
+
+          state = "Closed for Holiday";
+          stateClass = "isHoliday";
+          nextTime = ` Reopens ${nextMonthAndDate}`;
+          isNextTime = true;
+        } else if (bizDay === null) {
+          state = "Closed";
+          stateClass = "isClose";
+          // Reopen?
+        } else {
+          state = "Open";
+          stateClass = "isOpen";
+          // Check time range
+        }
       }
 
-      const holidays = this.store.businesshour[0].Holiday;
-      // console.log("Holidays: " + holidays); // ← 🐞 Debug console
-      // Extract holiday
-
-      // ↓ Check if the business is closed for the holiday
-      // const currentMonthandDay = currentMonth + currentDate;
-      // if (holidays.includes(currentMonthandDay)) {
-      //   return "Closed for Holiday";
-      // }
-
-      const currentMonthandDay = currentMonth + currentDate;
-      if (holidays.includes(currentMonthandDay)) {
-        // Check for next open day after holiday
-        let nextOpenDate = new Date(currentTime);
-        let nextMonthAndDate;
-        do {
-          nextOpenDate.setDate(nextOpenDate.getDate() + 1); // Increment date by 1
-          nextMonthAndDate =
-            String(nextOpenDate.getMonth() + 1).padStart(2, "0") +
-            String(nextOpenDate.getDate()).padStart(2, "0");
-        } while (holidays.includes(nextMonthAndDate));
-
-        return `Closed for Holiday. Opens on ${nextMonthAndDate}`;
-      }
-
-      // ↓ Check if closed today
-      const bizDay = this.store.businesshour[currentDay];
-      // console.log("Business Day: " + bizDay); // ← 🐞 Debug console
-      if (bizDay == null) {
-        return "Closed";
-      }
-    },
-    businessHoursClass() {
-      if (this.businessHours === "Closed for Holiday") {
-        return "closed-for-holiday";
-      } else if (this.businessHours === "Closed") {
-        return "closed";
-      } else if (this.businessHours === "Open 24H") {
-        return "opens";
-      }
-      return ""; // Default class if none of the conditions are met
+      return {
+        state: state,
+        time: time,
+        isTime: isTime,
+        nextTime: nextTime,
+        isNextTime: isNextTime,
+        stateClass: stateClass,
+        timeClass: timeClass,
+        nextTimeClass: nextTimeClass,
+      };
     },
   },
 };
 </script>
   
 <style scoped>
-.closed-for-holiday {
+.isHoliday {
   color: #ce53fa;
 }
 
-.closed {
+.isClose {
   color: #fd435f;
 }
 
-.opens {
+.isOpen {
   color: #3dc363;
   font-weight: 700;
 }
 </style>
-
