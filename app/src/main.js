8bd2da007132cede3f903152653e8fcc213233c5
ledtorch <jerry@morispace.com>
2023-09-09 20:40:31 +0800

    Add pinia and backup

--- app/src/main.js
+++ app/src/main.js
@@ -1,14 +1,22 @@
 import { createApp } from 'vue'
+
+// Components
 import App from './App.vue'
 import Map from "./components/Map.vue";
 import Detail from "./components/Detail.vue";
 
+// Plugin
 import router from './router.js'
+import { createPinia } from 'pinia';
 
 // Global CSS
 import './style.css'
 
 // Dependency
 import 'mapbox-gl/dist/mapbox-gl.css';
+
+// Initialize and use Pinia
+const pinia = createPinia();
+app.use(pinia);
 
 createApp(App).use(router).mount('#app')
