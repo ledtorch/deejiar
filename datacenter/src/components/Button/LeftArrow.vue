21af27aeb6c04c080f6c314fe0d0f1e3503413ac
ledtorch <jerry@morispace.com>
2024-06-15 10:40:17 +0800

    feat: arrow button

---
+++ datacenter/src/components/Button/LeftArrow.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="icon-button"></button>
+</template>
+
+<style lang="scss" scoped>
+.icon-button {
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  background: no-repeat center/contain;
+  background-image: url("/button/icon/contral-data/left-arrow-default.png");
+
+  // 🏗️ TODO: Doesn't work
+  &:active {
+    background-image: url("/button/icon/contral-data/left-arrow-click.png");
+  }
+
+  &:hover {
+    background-image: url("/button/icon/contral-data/left-arrow-hover.png");
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background-image: url("/button/icon/contral-data/left-arrow-inactive.png");
+  }
+}
+</style>
---
+++ datacenter/src/components/Button/RightArrow.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="icon-button"></button>
+</template>
+
+<style lang="scss" scoped>
+.icon-button {
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  background: no-repeat center/contain;
+  background-image: url("/button/icon/contral-data/right-arrow-default.png");
+
+  // 🏗️ TODO: Doesn't work
+  &:active {
+    background-image: url("/button/icon/contral-data/right-arrow-click.png");
+  }
+
+  &:hover {
+    background-image: url("/button/icon/contral-data/right-arrow-hover.png");
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background-image: url("/button/icon/contral-data/right-arrow-inactive.png");
+  }
+}
+</style>
