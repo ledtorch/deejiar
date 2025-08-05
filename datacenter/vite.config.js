4782f41fe0194e7ae40dee3cb303ba37783e564d
ledtorch <jerry@morispace.com>
2025-08-05 23:14:47 +0800

    feat: updated to SSL connection

--- datacenter/vite.config.js
+++ datacenter/vite.config.js
@@ -2,8 +2,20 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
+// HTTPS dependency
+import fs from 'fs'
+
 export default defineConfig({
   plugins: [vue()],
+
+  server: {
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./localhost.key'),
+      cert: fs.readFileSync('./localhost.crt'),
+    },
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
