59765e1d7e1da1933795a462f7a9d5f6efbdffc9
ledtorch <jerry@morispace.com>
2025-08-20 23:05:03 +0800

    wip: initialize

---
+++ datacenter/src/components/Button/Primary.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="icon-button _color-primary">{{ label }}</button>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: { type: String, default: '' },
+})
+</script>
+
+<style lang="scss" scoped>
+.icon-button {
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  background: no-repeat center/contain;
+  background-image: url("/button/icon/contral-data/right-arrow-default.png");
+
+  // 🏗️ TODO: Doesn't work
+  &:active {
+    background-image: url("/button/icon/contral-data/right-arrow-click.png");
+  }
+
+  &:hover {
+    background-image: url("/button/icon/contral-data/right-arrow-hover.png");
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background-image: url("/button/icon/contral-data/right-arrow-inactive.png");
+  }
+}
+</style>
