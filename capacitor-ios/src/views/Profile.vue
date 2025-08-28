23262aa388f51f19f5e713f0aad9928201ad9c5b
ledtorch <jerry@morispace.com>
2025-08-28 12:52:33 +0800

    chore: initiate profile page

---
+++ capacitor-ios/src/views/Profile.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="body">
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.body {
+  position: relative;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+
+  /* Media query for screen widths 430px and below */
+  @media (max-width: 430px) {
+    padding-bottom: 99px;
+  }
+}
+</style>
