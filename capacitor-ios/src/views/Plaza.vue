9129a9956d2913818744844f5f9a796d80611b81
ledtorch <jerry@morispace.com>
2025-08-29 13:33:03 +0800

    chore: initialize plaza page

---
+++ capacitor-ios/src/views/Plaza.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="body">
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.body {
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+
+  /* Media query for screen widths 430px and below */
+  @media (max-width: 430px) {
+    padding-bottom: 99px;
+  }
+}
+</style>
