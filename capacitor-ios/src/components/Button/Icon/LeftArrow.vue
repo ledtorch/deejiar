098f1736e36e26d3c9c62a516ddda92727ed2ede
ledtorch <jerry@morispace.com>
2025-08-30 15:10:11 +0800

    feat: add arrow

--- capacitor-ios/src/components/Button/Icon/LeftArrow.vue
+++ capacitor-ios/src/components/Button/Icon/LeftArrow.vue
@@ -2,32 +2,15 @@
   <button class="icon-button"></button>
 </template>
 
-<script>
-export default {
-  name: 'LeftArrow'
-};
+<script setup>
 </script>
 
 <style lang="scss" scoped>
 .icon-button {
   cursor: pointer;
-  width: 32px;
-  height: 32px;
+  width: 24px;
+  height: 24px;
   background: no-repeat center/contain;
-  background-image: url("/button/icon/with-base/left-arrow-default.png");
-
-  // 🏗️ TODO: Doesn't work
-  &:active {
-    background-image: url("/button/icon/with-base/left-arrow-click.png");
-  }
-
-  &:hover {
-    background-image: url("/button/icon/with-base/left-arrow-hover.png");
-  }
-
-  &:disabled {
-    cursor: not-allowed;
-    background-image: url("/button/icon/with-base/left-arrow-inactive.png");
-  }
+  background-image: url("/button/icon/control-data/left-arrow.svg");
 }
 </style>
