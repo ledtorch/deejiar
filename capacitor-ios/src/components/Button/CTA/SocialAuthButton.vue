4f3c4c36bdbb2fff2efcd702c21ca7eaa66fd35c
ledtorch <jerry@morispace.com>
2025-09-02 00:01:09 +0800

    feat(SocialAuthButton): initialize layout and basic logic

---
+++ capacitor-ios/src/components/Button/CTA/SocialAuthButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button v-if="instagram" :class="['button-container', '_button-secondary', { 'is-pressed': isPressed }]"
+    @pointerdown="onPressStart" @pointerup="onPressEnd">
+    <img src="/icon/social/instagram-color.png" class="auth-icon" />
+    {{ action }}</button>
+
+  <button v-else-if="x" :class="['button-container', '_button-secondary', { 'is-pressed': isPressed }]"
+    @pointerdown="onPressStart" @pointerup="onPressEnd">
+    <img src="/icon/social/x-color.png" class="auth-icon" />
+    {{ action }}</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  action: { type: String, default: '' },
+  instagram: { type: Boolean, default: false },
+  x: { type: Boolean, default: false },
+})
+
+const isPressed = ref(false)
+
+const onPressStart = (e) => {
+  isPressed.value = true;
+};
+const onPressEnd = () => {
+  isPressed.value = false;
+};
+
+</script>
+
+<style lang="scss" scoped>
+.button-container {
+  display: flex;
+  align-self: stretch;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  height: 44px;
+  padding: 0 var(--division);
+  gap: var(--atom);
+  border-radius: var(--round-m);
+  background: var(--content);
+  color: var(--primary-text);
+
+  /* Interactions */
+  transition: all 0.3s ease;
+
+  &.is-pressed {
+    background: transparent;
+  }
+}
+
+.auth-icon {
+  width: 24px;
+  height: 24px;
+}
+</style>
