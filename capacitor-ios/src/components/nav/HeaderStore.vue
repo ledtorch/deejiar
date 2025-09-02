a3815540bae684b487df403b61ee7ce674a1da0c
ledtorch <jerry@morispace.com>
2025-09-03 00:08:03 +0800

    feat(HeaderStore): initialize component

---
+++ capacitor-ios/src/components/nav/HeaderStore.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header-store">
+    <h4 class="store-title">{{ store.title }}</h4>
+    <TagShopType :store="store" />
+  </div>
+</template>
+
+<script setup>
+import TagShopType from '../button/TagShopType.vue';
+
+const props = defineProps({
+  store: { type: Object, default: () => ({}) }
+})
+</script>
+
+<style lang="scss" scoped>
+.header-store {
+  width: 100%;
+  align-items: top;
+  justify-content: space-between;
+  gap: var(--block);
+}
+
+.store-title {
+  flex: 1 0 0;
+}
+</style>
