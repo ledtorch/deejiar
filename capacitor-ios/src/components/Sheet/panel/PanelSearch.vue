8d154243fd7a5044956b343dee1f3e14184bb3f8
ledtorch <jerry@morispace.com>
2025-09-13 03:06:09 +0800

    wip: update layout

--- capacitor-ios/src/components/Sheet/panel/PanelSearch.vue
+++ capacitor-ios/src/components/Sheet/panel/PanelSearch.vue
@@ -4,12 +4,44 @@
       <TheSearch />
       <TheAvatar class="avatar-layout" userState="default" uphoto="..." uid="..." :useBottomSheet="true" />
     </div>
+    <div class="collection-container">
+      <p class="_subtitle collection-title">Deejiar Collections</p>
+      <ListCollection cocktail />
+      <ListCollection icecream />
+      <ListCollection taco />
+    </div>
+    <PrimaryButton :action="CTAButtonText" type="default" @click="handleLoginOrRegister" />
   </section>
 </template>
 
 <script setup>
 import TheSearch from '../../TheSearch.vue';
 import TheAvatar from '../../button/TheAvatar.vue';
+import PrimaryButton from '../../button/CTA/PrimaryButton.vue';
+import ListCollection from '../../list/ListCollection.vue';
+import { inject, computed } from 'vue';
+import { useUserStore } from '../../../stores/userStore';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const userStore = useUserStore();
+const bottomSheetControls = inject('bottomSheetControls', null);
+
+const handleLoginOrRegister = () => {
+  if (userStore.userState === 'default') {
+    bottomSheetControls.switchPanel('auth');
+  } else if (userStore.userState === 'active') {
+    router.push({ name: 'subscription' });
+  }
+};
+
+const CTAButtonText = computed(() => {
+  if (userStore.userState === 'default') {
+    return 'Log in to Unlock Advanced Features';
+  } else if (userStore.userState === 'active') {
+    return 'Unlock Advanced Features';
+  }
+});
 </script>
 
 <style lang="scss" scoped>
@@ -27,5 +59,15 @@ import TheAvatar from '../../button/TheAvatar.vue';
 .avatar-layout {
   width: 36px;
   height: 36px;
+}
+
+.collection-container {
+  flex-direction: column;
+  gap: var(--box);
+  padding: var(--block) 0 var(--wrapper) 0;
+}
+
+.collection-title {
+  color: var(--tertiary-text);
 }
 </style>
