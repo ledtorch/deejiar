58f661c9016eb569d1f9833a5cefb41ac27e6536
ledtorch <jerry@morispace.com>
2025-09-14 20:27:37 +0800

    feat: add variable close-item

--- capacitor-ios/src/components/Button/Icon/Close.vue
+++ capacitor-ios/src/components/Button/Icon/Close.vue
@@ -1,17 +1,39 @@
 <template>
-  <button class="icon-button" @click="onClick"></button>
+  <button :class="buttonClass" @click="onClick"></button>
 </template>
 
 <script setup>
-const emit = defineEmits(['close']);
+import { computed } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'item',
+    validator: (value) => ['item', 'modal'].includes(value)
+  }
+})
+
+const emit = defineEmits(['close'])
+
+const buttonClass = computed(() => {
+  return props.type === 'modal' ? 'close-modal' : 'close-item'
+})
 
 const onClick = () => {
-  emit('close');
-};
+  emit('close')
+}
 </script>
 
 <style lang="scss" scoped>
-.icon-button {
+.close-item {
+  cursor: pointer;
+  min-width: 24px;
+  min-height: 24px;
+  background: no-repeat center/contain;
+  background-image: url("/button/icon/control/close-item.svg");
+}
+
+.close-modal {
   cursor: pointer;
   min-width: 32px;
   min-height: 32px;
