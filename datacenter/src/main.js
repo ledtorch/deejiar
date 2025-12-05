9ca354da0e6d11046f67f5bc38540569e5aac613
ledtorch <jerry@morispace.com>
2025-12-05 16:54:47 +0800

    refactor: 1. remove tailwind 2. migrate to pnpm

--- datacenter/src/main.js
+++ datacenter/src/main.js
@@ -1,17 +1,11 @@
 import { createApp } from 'vue'
 
-// Components
 import App from './App.vue'
-
-// Plugin
 import router from './router'
 
-// Global CSS and Tailwind CSS
-import './assets/tailwind.css';
 import './style.css'
 
 const app = createApp(App)
 
 app.use(router)
-
 app.mount('#app')
